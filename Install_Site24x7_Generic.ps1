932f966a5634697352de233dce141eafaf77ee35
Luke <lukealdred86@hotmail.com>
2021-01-26 13:34:44 +0000

    Add files via upload

---
+++ Install_Site24x7_Generic.ps1
@@ -0,0 +1,18 @@
+﻿#Create download directory
+mkdir C:\site24x7\
+
+# Download package
+Invoke-WebRequest https://staticdownloads.site24x7.eu/server/Site24x7WindowsAgent.msi -OutFile C:\site24x7\Site24x7WindowsAgent.msi
+
+# Switch location
+Set-Location C:\site24x7\
+
+# Run installation
+Start-Process -FilePath msiexec.exe -ArgumentList '/i Site24x7WindowsAgent.msi EDITA1= ENABLESILENT=YES REBOOT=ReallySuppress TP="Jisc Cloud default Server Monitor thresholds" NP="Default Notification" GN="" /qn'
+
+#Wait a minute
+ping -n 60 127.0.0.1 > wait.txt
+
+#Clean up
+del Site24x7WindowsAgent.msi
+del wait.txt
